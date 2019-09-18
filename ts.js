e1d4a2cebcb8b1de0d103f7d62baf371b294451f
Daniel Masters <daniel.p.masters@gmail.com>
2019-09-18 11:45:34 -0500

    added index.hmtl and ts.js

---
+++ ts.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDZEs3ddtP2PaskZlDtPdgLB6-IJbVGx5Y",
+    authDomain: "timesheet-project-cc6c1.firebaseapp.com",
+    databaseURL: "https://timesheet-project-cc6c1.firebaseio.com",
+    projectId: "timesheet-project-cc6c1",
+    storageBucket: "",
+    messagingSenderId: "403747809066",
+    appId: "1:403747809066:web:2b06ff4b5e04212b636053"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
